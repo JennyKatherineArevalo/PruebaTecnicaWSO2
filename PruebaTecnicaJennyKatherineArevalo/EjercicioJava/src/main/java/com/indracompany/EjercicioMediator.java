0fec6fd6a86542cd4aa959e6b1f229387803a9b9
Jenny Katherine Arevalo <jen.arevalo@hotmail.es>
2021-09-22 11:34:35 -0500

    Doc prueba tecnica

---
+++ PruebaTecnicaJennyKatherineArevalo/EjercicioJava/src/main/java/com/indracompany/EjercicioMediator.java
@@ -0,0 +1,12 @@
+package com.indracompany;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class EjercicioMediator extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		return true;
+	}
+}
